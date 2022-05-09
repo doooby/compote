84fa2054a74fcd5b036b5310d71115f223df917c
doooby <zelazk.o@email.cz>
2022-05-09 02:19:20 +0200

    dev

--- lib/compote/jar/docker.rb
+++ lib/compote/jar/docker.rb
@@ -40,7 +40,7 @@ docker run --rm \
   --env-file .env \
   #{options.join '   '} \
   #{image_tag image} \
-  #{command.strip}
+   bash -c "#{command.strip.gsub '"' '\"'}"
         COMMAND
       end
 
